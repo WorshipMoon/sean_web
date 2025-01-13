5f693eca3d23374c663187387bf9afa4a84dc083
Moon <moon@163.com>
2025-01-13 14:49:29 +0800

    googlemaps

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,17 +1,44 @@
 <script setup>
-import { useData } from "vitepress";
-import NotFound from "./NotFound.vue";
-import Home from "./Home.vue";
-// import Page from './Page.vue'
+import DefaultTheme from "vitepress/theme";
+import { onMounted } from "vue";
 
-const { page, frontmatter } = useData();
+const { Layout } = DefaultTheme;
+
+onMounted(() => {
+  // 确保在客户端环境下运行
+  if (typeof window !== "undefined") {
+    window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
+    // setTimeout(() => {
+    //   const s1 = document.createElement("script");
+    //   const s0 = document.getElementsByTagName("script")[0];
+
+    //   s1.async = true;
+    //   s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
+    //   s1.charset = "UTF-8";
+    //   s1.setAttribute("crossorigin", "*");
+
+    //   if (s0 && s0.parentNode) {
+    //     s0.parentNode.insertBefore(s1, s0);
+    //   } else {
+    //     document.head.appendChild(s1);
+    //   }
+    // }, 0);
+
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }
+});
 </script>
 
 <template>
-    <div>123</div>
-    <Content />
-    <!-- <NotFound v-if="page.isNotFound" />
-    <Home v-if="frontmatter.layout === 'home'" />
-    <Page v-else />
-    <Page /> renders <Content /> -->
+  <Layout />
 </template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,42 @@
 import DefaultTheme from "vitepress/theme";
 // import MyLayout from "./Layout.vue";
-import { h, watch } from "vue";
+import { h, watch, onMounted, nextTick } from "vue";
+import { useRoute } from "vitepress";
 import "./style/index.css";
 let homePageStyle: HTMLStyleElement | undefined;
+import Layout from "./Layout.vue";
+import mediumZoom from "medium-zoom";
 
 export default {
-  extends: DefaultTheme,
+  // extends: DefaultTheme,
+  Layout,
   // Layout() {
   //   return h(DefaultTheme.Layout, null, {
   //     "aside-outline-before": () => h(MyLayout),
   //   });
   // },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom("[data-zoomable]", { background: "var(--vp-c-bg)" });
+      // mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+    };
+
+    onMounted(() => {
+      initZoom();
+    });
+
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+  markdown: {
+    image: {
+      // 默认禁用；设置为 true 可为所有图片启用懒加载。
+      lazyLoading: true,
+    },
+  },
   enhanceApp({ app, router, siteData }) {
     // ...
     if (typeof window === "undefined") return;
@@ -21,7 +47,6 @@ export default {
       () => updateHomePageStyle(true),
       { immediate: true }
     );
-
   },
 };
 function updateHomePageStyle(value: boolean) {
