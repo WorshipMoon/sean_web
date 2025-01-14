628cb68cbef6c7498db8c4360c61ed17f6a1a0d1
Moon <moon@163.com>
2025-01-14 09:16:39 +0800

    Comment customer service code

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -6,36 +6,36 @@ const { Layout } = DefaultTheme;
 
 onMounted(() => {
   // 确保在客户端环境下运行
-  if (typeof window !== "undefined") {
-    window.Tawk_API = window.Tawk_API || {};
-    window.Tawk_LoadStart = new Date();
+  // if (typeof window !== "undefined") {
+  //   window.Tawk_API = window.Tawk_API || {};
+  //   window.Tawk_LoadStart = new Date();
 
-    // setTimeout(() => {
-    //   const s1 = document.createElement("script");
-    //   const s0 = document.getElementsByTagName("script")[0];
+  //   // setTimeout(() => {
+  //   //   const s1 = document.createElement("script");
+  //   //   const s0 = document.getElementsByTagName("script")[0];
 
-    //   s1.async = true;
-    //   s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
-    //   s1.charset = "UTF-8";
-    //   s1.setAttribute("crossorigin", "*");
+  //   //   s1.async = true;
+  //   //   s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
+  //   //   s1.charset = "UTF-8";
+  //   //   s1.setAttribute("crossorigin", "*");
 
-    //   if (s0 && s0.parentNode) {
-    //     s0.parentNode.insertBefore(s1, s0);
-    //   } else {
-    //     document.head.appendChild(s1);
-    //   }
-    // }, 0);
+  //   //   if (s0 && s0.parentNode) {
+  //   //     s0.parentNode.insertBefore(s1, s0);
+  //   //   } else {
+  //   //     document.head.appendChild(s1);
+  //   //   }
+  //   // }, 0);
 
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-  }
+  //   (function () {
+  //     var s1 = document.createElement("script"),
+  //       s0 = document.getElementsByTagName("script")[0];
+  //     s1.async = true;
+  //     s1.src = "https://embed.tawk.to/6778dc1849e2fd8dfe024847/1igo1ngc3";
+  //     s1.charset = "UTF-8";
+  //     s1.setAttribute("crossorigin", "*");
+  //     s0.parentNode.insertBefore(s1, s0);
+  //   })();
+  // }
 });
 </script>
 
